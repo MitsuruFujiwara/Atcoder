8b12c9c71f6b13f5d18d6f310e565fc1a4770621
MitsuruFujiwara <fujiwara52jp@gmail.com>
2020-04-05 22:03:24 +0900

    add abc136

---
+++ abc136/B.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+  string N;
+  cin >> N;
+  cout << max(0,C-(A-B));
+  return 0;
+}
---
+++ make_files_cpp.sh
@@ -0,0 +1,8 @@
+mkdir $1
+cd $1
+echo -n > A.cpp
+echo -n > B.cpp
+echo -n > C.cpp
+echo -n > D.cpp
+echo -n > E.cpp
+echo -n > F.cpp
